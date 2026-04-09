9a053dd8ecdc0ac30476b93f21c7d130e0bc2f5b
AnishwarDutton <anishwarshar98@gmail.com>
2026-04-09 22:56:49 +0530

    Force component scan fix

--- newLPG/backend/src/main/java/com/restaurant/lpgmanagement/LpgManagementApplication.java
+++ newLPG/backend/src/main/java/com/restaurant/lpgmanagement/LpgManagementApplication.java
@@ -3,7 +3,7 @@ package com.restaurant.lpgmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.restaurant.lpgmanagement")
 public class LpgManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(LpgManagementApplication.class, args);
